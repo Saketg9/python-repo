e275f5648893c8803efad59f005d699410437efd
Saket Ghai <saket_g@hotmail.co.uk>
2021-01-06 11:26:58 +0000

    Issue #4: substract function added

---
+++ function4.py
@@ -0,0 +1,21 @@
+def hello_world(num1, num2):            ### num1 = 36 , num2 = 77
+    sum = num1 + num2                   ### sum = 36 + 77 
+    return sum                          ### return 113
+
+def sub(num1, num2):                    ### num1 = 36 , num2 = 77
+    sub = num1 - num2                   ### sum = 36 + 77 
+    return sub
+
+def get_value():
+    num = int(input('Enter a value')) 
+    return num
+
+val1 = get_value()                      ### val1 = 36
+val2 = get_value()                      ### val2 = 77
+
+answer = hello_world(val1, val2)                    ### hello_world(36, 77)  answer = 113
+print('The answer of addition is ' + str(answer))   
+
+
+answer = sub(val1, val2)                            ### hello_world(36, 77)  answer = 113
+print('The answer of subtraction  is ' + str(answer))
